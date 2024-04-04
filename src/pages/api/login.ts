4f7b1ea27495b7b2f1b7d5d4432ca73009a0017a
Дашноров дамдиндорж <23LP9512@G401-CC0213.local>
2024-04-04 12:45:39 +0800

    class

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,30 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from "nextjs-cors";
+import connect from "./database";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { loginService } from "@/services/user";
 
 type Data = {
   message?: string;
-  token?: string;
+  token?: string | any;
+  user?: string[];
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  await connect();
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
   const data = req.body;
-  const parsedData = JSON.parse(data);
-  console.log(parsedData);
-  res.status(200).json({ message: "hello" });
-  //   if (req.method !== "POST") {
-  //     return res.status(405).json({ message: "Method Not Allowed" });
-  //   }
-  //   const { email, password } = req.body;
-  //   try {
-  //     const token = await loginService(email, password);
-  //     if (token) {
-  //       return res.status(200).json({ token, message: "Login successful" });
-  //     }
-  //   } catch (e: any) {
-  //     return res.status(400).json({ message: e.message });
-  //   }
+  const { email, password } = data;
+  console.log(data);
+
+  try {
+    const token = await loginService(email, password);
+    if (token) {
+      return res
+        .status(200)
+        .json({ token: token, message: "Login successful" });
+    }
+  } catch (e: any) {
+    return res.status(400).json({ message: e.message });
+  }
 }
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,17 +1,6 @@
 import { UserModel } from "@/models/user.shcema";
-import { generateJwtToken } from "@/utils/generate-token";
-
-// export const loginService = async (email: string, password: number) => {
-//   if (email == "bla@gmail.com" && password == "bla") {
-//     const userInfo = {
-//       email: email,
-//       name: "dash",
-//     };
-//     return generateJwtToken(userInfo);
-//   } else {
-//     throw new Error("invalid credentials");
-//   }
-// };
+import { stat } from "fs";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (
   name: String,
@@ -21,4 +10,26 @@ export const createUser = async (
 ) => {
   const createUser = UserModel.create({ name, adress, email, password });
   return createUser;
+};
+
+export const loginService = async (email: string, password: string) => {
+  console.log(email, password);
+  try {
+    const user = await UserModel.findOne({ email, password });
+    console.log(user);
+    if (email == user.email && password == user.password) {
+      const userInfo = {
+        email: email,
+        name: password,
+      };
+      const newToken = jwt.sign(userInfo, "my-super-duper-secret-key", {
+        expiresIn: "1h",
+      });
+      return newToken;
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (e: any) {
+    return new Error(e.message);
+  }
 };
